7f23426bab78b5ef54f88da32004aa7cec469b82
Ruben Toledo <tibamita@tutamail.com>
2024-12-27 19:26:07 -0500

    added file identifier functions

--- filetypes.c
+++ filetypes.c
@@ -1,60 +1,99 @@
 #include "filetypes.h"
 
-#include <mutex>
-#include <string.h>
-#include <stdio.h>
+#include <complex.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <assert.h>
 #include <errno.h>
 
-typedef struct FT_Magic {
-    char elf[4];
-    char png[8];
-    char pdf[7];
-} ft_magics;
+bool ft_is_elf(FILE *fp) {
+    fseek(fp, 0, SEEK_SET); // read the right bytes
 
-ft_magics* init_magics() {
-    ft_magics *ret = (ft_magics *)malloc(sizeof(ft_magics));
-    if (!ret) {
-        perror("Malloc");
-        exit(errno);
-    }
+    char elf_mag[ELF_MAG_LEN] = {0x7f, 'E', 'L', 'F'};
+    char buf[ELF_MAG_LEN] = {};
+
+    fread(buf, sizeof(buf), 1, fp);
     
-    char elf[4] = {0x7f, 'E', 'L', 'F'};
-    char png[8] = {0x89, 'P', 'N', 'G', 0x0d, 0x0a, 0x1a, 0x0a};
-    char pdf[7] = {'%', 'P', 'D', 'F', '-', '1', '.'};
-
-    strcpy(ret->elf, elf);
-    strcpy(ret->png, png);
-    strcpy(ret->pdf, pdf);
-
-    return ret;
+    for (int i = 0; i < ELF_MAG_LEN; i++)
+        if (buf[i] != elf_mag[i]) return false;
+    return true;
 }
 
-int get_type(FILE *fp) {
-    ft_magics *magic = init_magics();
-    char c;
-    int k;
-    for (int i = 0; i < NMAGICNUMS; i++) {
-        k = 0;
-        do {
-            c = fgetc(fp);
-            if (c != magics[i][k]) break; 
-            if (k == strlen(magics[i])) return i;
-            k++; 
-        } while (c != EOF);
-    }
-    free(magics);
-    return -1;
+bool ft_is_png(FILE *fp) {
+    fseek(fp, 0, SEEK_SET); // read the right bytes
+
+    char png_mag[PNG_MAG_LEN] = {0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a};
+    char buf[PNG_MAG_LEN] = {};
+
+    fread(buf, sizeof(buf), 1, fp);
+
+    for (int i = 0; i < PNG_MAG_LEN; i++)
+        if (buf[i] != png_mag[i]) return false;
+    return true;
 }
 
-file_t* open_file(const char *fname) {
-    file_t *file = (file_t *)malloc(sizeof(file_t));
-    if (!file) return NULL;
-    file->fp = fopen(fname, "rb");
-    if (!file->fp) {
-        free(file);
-        return NULL;
+bool ft_is_pdf(FILE *fp) {
+    fseek(fp, 0, SEEK_SET); // read the right bytes 
+
+    char pdf_mag[PDF_MAG_LEN - 1] = {'%', 'P', 'D', 'F', '-', '1', '.'};
+    char buf[PDF_MAG_LEN] = {};
+
+    fread(buf, sizeof(buf), 1, fp);
+
+    for (int i = 0; i < PDF_MAG_LEN - 1; i++)
+        if (buf[i] != pdf_mag[i]) return false;
+
+    /* check if it's a valid version */
+    if (buf[sizeof(buf) - 1] >= '0' && buf[sizeof(buf) - 1] <= '7')
+        return true;
+
+    return false;
+}
+
+int ft_determine_filetype(FILE *fp) {
+    if (ft_is_elf(fp)) return FT_ELF;
+    if (ft_is_png(fp)) return FT_PNG;
+    if (ft_is_pdf(fp)) return FT_PDF;
+    return FT_UNKNOWN; /* always return a value */
+}
+
+file_inf ft_get_file_info(const char *fname) {
+    assert(fname);
+    file_inf finf;
+    
+    /* open the file */
+    finf.fp = fopen(fname, "r");
+    if (!finf.fp) {
+        if (errno == ENOENT) {
+            printf("No such file: %s\n", fname);
+            exit(-errno);
+        }
+        else if (errno == EACCES) {
+            printf("Permission denied.\nRun with sudo to access %s\n", fname);
+            exit(-errno);
+        }
+        else {
+            perror("fopen");
+            exit(-errno);
+        }
     }
-    file->ftype = get_type(file->fp);
-    return file;
+
+    finf.fname = fname;
+
+    /* get file system info about file */
+    if (lstat(fname, &finf.fstat) == -1) {
+        perror("lstat");
+        exit(-errno);
+    }
+
+    /* determine if is dir */
+    if (S_ISDIR(finf.fstat.st_mode))
+        finf.isdir = true;
+    else finf.isdir = false;    
+
+    finf.filetype = ft_determine_filetype(finf.fp);
+
+    return finf;
 }
